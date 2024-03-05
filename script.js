390b2f8b7a8e19552eaa169c5c9cd0cbdfd13cbf
gogulnath63 <160810296+gogulnath63@users.noreply.github.com>
2024-03-05 22:45:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let myResume={
+    "basics": {
+      "name": "GOGULNATH B",
+      "email": "gogulnath6383@gamil.com",
+      "phone": 6383761578,
+      "degree": "Bsc",
+      "location": {
+        "address": "2/166 vaaniyarveethi anathur villupuram",
+        "postalCode": 607101,
+        "city": "villupuram",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      
+    
+    "education": [
+      {
+        "institution": "joshep arts and science collage",
+        "department": "Bsc botany",
+        "studyType": "fulltime",
+        "batch start year": 2019,
+        "batch end year": 2022
+        
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    
+  console.log(myResume);
